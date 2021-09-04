b07e2f66d2e5c82e0bfa6271228dde116475f7a1
TTHLrnd <lorandtoth.lori@gmail.com>
2021-09-04 16:17:32 +0200

    Add description Add player class and model package

--- rpg/src/main/java/Main.java
+++ rpg/src/main/java/Main.java
@@ -1,2 +1,43 @@
 public class Main {
+
+    /**
+     * player
+     *  	username VARCHAR(50), -- szöveg nem lehet AUTO_INCREMENT!!!!!!!!!!!!!
+     * 	    email_address VARCHAR(100) UNIQUE,
+     * 	    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     * 	    PRIMARY KEY (username)
+     *
+     * playable character
+     *      character_id INT UNSIGNED AUTO_INCREMENT,
+     *      character_name VARCHAR(50),
+     * 	    character_class ENUM(
+     * 		    'mage',		-- 1
+     *         'warrior',	-- 2
+     *         'rogue',	-- 3
+     *         'priest',	-- 4
+     *         'shaman'	-- 5
+     *     ),
+     * 	    character_race ENUM(
+     * 		'human',
+     *         'orc',
+     *         'elf',
+     *         'undead',
+     *         'halfling'
+     *     ),
+     * 	character_img BLOB,
+     * 	character_skill1 VARCHAR(50),
+     * 	character_skill2 VARCHAR(50),
+     * 	character_skill3 VARCHAR(50),
+     *     PRIMARY KEY (character_id)
+     *
+     *
+     * game session
+     *  	id INT AUTO_INCREMENT,
+     * 	    game_end_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     * 	    PRIMARY KEY (id)
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
 }
